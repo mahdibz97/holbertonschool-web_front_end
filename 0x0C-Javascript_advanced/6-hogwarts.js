a0ca42442bf8a9b2871f65351b1779f38ca151ff
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-03-09 23:38:14 +0100

    JS advanced

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,34 @@
+function studentHogwarts() {
+    let privateScore = 0;
+    let name = null;
+
+    let changeScoreBy = function(points) {
+        privateScore = privateScore + points;
+    };
+
+    return {
+        setName: function(newName) {
+            name = newName;
+        },
+        rewardStudent: function() {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function() {
+            changeScoreBy(-1);
+        },
+        getScore: function() {
+            return name + ": " + privateScore;
+        },
+    }
+}
+
+let harry = studentHogwarts();
+harry.setName("Harry");
+for (let i=0; i < 4; i++) harry.rewardStudent();
+console.log(harry.getScore());
+
+let draco = studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+for (let i=0; i < 3; i++) draco.penalizeStudent();
+console.log(draco.getScore());
